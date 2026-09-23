a0409a9943e3f096c63a15f52a2bf32f2e4c6d3e
mosharrof0000-ux <mosharrof0000@gmail.com>
2026-09-23 19:49:10 +0600

    Fix API path normalization

--- backend/api-v1/validation.js
+++ backend/api-v1/validation.js
@@ -1,8 +1,6 @@
 export function positiveInt(value, name, { min = 1, max = 1000 } = {}) {
   const n = Number(value);
-  if (!Number.isInteger(n) || n < min || n > max) {
-    throw new Error(name + '_INVALID');
-  }
+  if (!Number.isInteger(n) || n < min || n > max) throw new Error(name + '_INVALID');
   return n;
 }
 
@@ -13,5 +11,5 @@ export function boundedQuery(value, { max = 200 } = {}) {
 }
 
 export function pathParts(pathname) {
-  return pathname.replace(/^\\/+|\\/+$/g, '').split('/').filter(Boolean);
+  return pathname.replace(/^\/+|\/+$/g, '').split('/').filter(Boolean);
 }
